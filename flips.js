605453a6ad74e44cf567f209e42083dac8b0f2d7
Cecilia (Chae Hyun) Lee <chaelee@email.unc.edu>
2022-02-11 09:30:35 -0500

    pls?

--- flips.js
+++ flips.js
@@ -2,12 +2,23 @@ import * as Coin from "./modules/coin.mjs";
 import { createRequire } from 'module';
 const require = createRequire(import.meta.url);
 const args = require('minimist')(process.argv.slice(2));
-args["number"]
-const number = args.number || 1;
-//let number = String(args).substring(9, 11);
+args['number']
+const number = args.number;
+//console.log(number);
 const coinArray = [];
-for (let i = 0; i < number; i++) {
-    coinArray[i] = Coin.coinFlip();
+if (number == null) {
+    let result = Coin.coinFlips(1);
+    console.log(result);
+    if (result == [ 'tails' ]) {
+        console.log("{ tails: 1 }");
+    } else {
+        console.log("{ heads: 1 }");
+    }
+} else {
+    for (let i = 0; i < number; i++) {
+      coinArray[i] = Coin.coinFlip();
+    }
+    console.log(coinArray);
+    console.log(Coin.countFlips(coinArray));
 }
-console.log(coinArray);
-console.log(Coin.countFlips(coinArray));
+
